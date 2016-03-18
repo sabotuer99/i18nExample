17167791d2016adfd18390a814c01ba76a084c47
sabotuer99 <sabotuer99@gmail.com>
2016-03-18 14:38:44 -0600

    i18n examples

---
+++ Internationalization/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Internationalization.Startup))]
+namespace Internationalization
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
